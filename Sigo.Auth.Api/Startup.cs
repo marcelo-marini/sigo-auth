ef392b3d81d4929ece79f75369bc43ff65939d46
Marcelo Marini <marcelomarini17@hotmail.com>
2021-04-08 00:00:07 -0300

    :art: Ajustes

--- Sigo.Auth.Api/Startup.cs
+++ Sigo.Auth.Api/Startup.cs
@@ -85,11 +85,11 @@ namespace Sigo.Auth.Api
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                app.UseHttpsRedirection();
             }
+
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseRouting();
