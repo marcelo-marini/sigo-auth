d67e10b3b89a241869fcc7ae883316eff1a76b6f
Marcelo Marini <marcelomarini17@hotmail.com>
2021-04-08 01:02:22 -0300

    Ajuste

--- Sigo.Auth.Api/Startup.cs
+++ Sigo.Auth.Api/Startup.cs
@@ -78,6 +78,11 @@ namespace Sigo.Auth.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Lax
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
