aea9046c31c3fc9a217e5ec50f25dab7af368721
Marcelo Marini <marcelomarini17@hotmail.com>
2021-04-12 20:38:59 -0300

    :art: Melhorias

---
+++ Sigo.Auth.Api/Data/Extensions/IdentityExtensions.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Sigo.Auth.Api.Data.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IApplicationBuilder InitializeDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+
+            if (serviceScope == null) return app;
+
+            var configurationDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+            if (!configurationDbContext.Clients.Any())
+            {
+                foreach (var client in ConfigurationDbSeed.Clients)
+                {
+                    if (!configurationDbContext.Clients.Any(x => x.ClientId == client.ClientId))
+                        configurationDbContext.Clients.Add(client.ToEntity());
+                }
+
+                configurationDbContext.SaveChanges();
+            }
+
+            if (!configurationDbContext.IdentityResources.Any())
+            {
+                foreach (var resource in ConfigurationDbSeed.IdentityResources)
+                {
+                    configurationDbContext.IdentityResources.Add(resource.ToEntity());
+                }
+
+                configurationDbContext.SaveChanges();
+            }
+
+            if (!configurationDbContext.ApiScopes.Any())
+            {
+                foreach (var apiScope in ConfigurationDbSeed.ApiScopes)
+                {
+                    configurationDbContext.Add(apiScope.ToEntity());
+                }
+
+                configurationDbContext.SaveChanges();
+            }
+
+            return app;
+        }
+    }
+}
--- Sigo.Auth.Api/Startup.cs
+++ Sigo.Auth.Api/Startup.cs
@@ -1,3 +1,4 @@
+using IdentityModel;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sigo.Auth.Api.Data;
+using Sigo.Auth.Api.Data.Extensions;
 using Sigo.Auth.Api.Models;
 using System;
 using System.Linq;
@@ -119,47 +121,7 @@ namespace Sigo.Auth.Api
 
             app.UseEndpoints(endpoints => { app.UseEndpoints(builder => { builder.MapDefaultControllerRoute(); }); });
 
-            InitializeDatabase(app);
-        }
-
-        private static void InitializeDatabase(IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
-
-            if (serviceScope == null) return;
-
-            var configurationDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-
-            if (!configurationDbContext.Clients.Any())
-            {
-                foreach (var client in ConfigurationDbSeed.Clients)
-                {
-                    if (!configurationDbContext.Clients.Any(x => x.ClientId == client.ClientId))
-                        configurationDbContext.Clients.Add(client.ToEntity());
-                }
-
-                configurationDbContext.SaveChanges();
-            }
-
-            if (!configurationDbContext.IdentityResources.Any())
-            {
-                foreach (var resource in ConfigurationDbSeed.IdentityResources)
-                {
-                    configurationDbContext.IdentityResources.Add(resource.ToEntity());
-                }
-
-                configurationDbContext.SaveChanges();
-            }
-
-            if (!configurationDbContext.ApiScopes.Any())
-            {
-                foreach (var apiScope in ConfigurationDbSeed.ApiScopes)
-                {
-                    configurationDbContext.Add(apiScope.ToEntity());
-                }
-
-                configurationDbContext.SaveChanges();
-            }
+            app.InitializeDatabase();
         }
     }
 }
